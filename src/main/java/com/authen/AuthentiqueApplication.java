32cce6731a92e7094ca8790a184fa8acdd86278f
Hamid Nazari <nazarihamid490@gmail.com>
2023-09-02 14:21:18 +0200

    First commit

---
+++ src/main/java/com/authen/AuthentiqueApplication.java
@@ -0,0 +1,13 @@
+package com.authen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentiqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authen/AuthentiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
